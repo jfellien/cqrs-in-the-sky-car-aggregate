2b38b42ab43d155d77d0989b75d27db0f54ae3f3
Jan Fellien <jan.fellien@devcrowd.de>
2017-09-15 20:40:13 +0200

    splited the output

--- Rent/index.js
+++ Rent/index.js
@@ -3,18 +3,20 @@ const uuid = require('uuid/v4');
 module.exports = (context, command, history) => {
   // use the history (Event Stream) for business decisions
   
-  context.bindings.events = [
-    {
-      eventName: "car rented",
-      carId: command.carId
-    },
-    {
-      eventName: "journey prepared",
-      carId: command.carId,
-      journeyId: uuid(),
-      driverId: command.driverId
-    }
-  ]
+  const carRented = {
+    eventName: "car rented",
+    carId: command.carId
+  };
+
+  const journeyPrepared = {
+    eventName: "journey prepared",
+    carId: command.carId,
+    journeyId: uuid(),
+    driverId: command.driverId
+  };
+
+  context.bindings.notifications = [ carRented, journeyPrepared ];
+  context.bindings.carRented = carRented;
 
   context.done();
 }
